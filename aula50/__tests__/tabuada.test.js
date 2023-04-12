25c790e48543f0cb5394c8eb8c6514e65f01ecb0
ursarah <samthvet@hotmail.com>
2023-04-12 11:51:18 -0300

    atualizando a aula

--- aula50/__tests__/tabuada.test.js
+++ aula50/__tests__/tabuada.test.js
@@ -11,3 +11,31 @@ test('garante que somar numeros negativos retorna valor correto', () => {
 test('garante somar consiga lidar com numeros reais', () => {
   expect(tabuada.somar(8.1, 10.6)).toBe(18.7);
 });
+
+test('garante subtracao adequada', () => {
+  expect(tabuada.subtrair(6, 5)).toBe(1);
+});
+
+test('garante subtracao adequada', () => {
+  expect(tabuada.subtrair(6.6, 5.1)).toBe(1.5);
+});
+
+test('garante divisao adequada', () => {
+  expect(tabuada.divisao(4, 2)).toBe(2);
+});
+
+test('garante divisao adequada', () => {
+  expect(tabuada.divisao(4, 0)).toBe("n2 não pode ser 0");
+});
+
+test('garante divisao adequada', () => {
+  expect(tabuada.divisao(4.4, 2)).toBe(2.2);
+});
+
+test('garante multiplicacao adequada', () => {
+  expect(tabuada.multiplicar(4, 2)).toBe(8);
+});
+
+test('garante multiplicacao adequada', () => {
+  expect(tabuada.multiplicar(4.6, 3.1)).toBe(14.26);
+});
--- aula50/src/tabuada.js
+++ aula50/src/tabuada.js
@@ -3,6 +3,30 @@ function somar (n1, n2){
   return Number(soma.toFixed(4));
 }
 
+function subtrair(n1, n2){
+  let resultado = n1 - n2;
+
+  return Number(resultado.toFixed(2));
+};
+
+function divisao(n1, n2){
+  if(n2 !== 0){
+    let resultado = n1 / n2;
+    return Number(resultado.toFixed(2));
+  }
+
+  return "n2 não pode ser 0";
+  
+};
+
+function multiplicar(n1, n2){
+  let resultado = n1 * n2;
+  return Number(resultado.toFixed(2));
+}
+
 module.exports = {
-  somar
+  somar,
+  subtrair,
+  divisao,
+  multiplicar
 };
